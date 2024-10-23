313c3f9c232dce8a928c11f6d097b0a907064037
eliasjuk <eliasp.juk@hotmail.com>
2024-10-22 21:20:47 -0300

    add rota pessoa e iniciar server

--- api/src/main/java/com/rinha/Main.java
+++ api/src/main/java/com/rinha/Main.java
@@ -1,7 +1,20 @@
 package com.rinha;
 
+import java.io.IOException;
+
+import com.rinha.api.PessoaHandler;
+import com.rinha.api.SimpleRestServer;
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		try {
+			SimpleRestServer server = new SimpleRestServer(8080);
+			server.addRoute("/pessoas", new PessoaHandler());
+			server.start();
+
+			// Adiciona um shutdown hook para parar o servidor ao encerrar o programa
+			Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
