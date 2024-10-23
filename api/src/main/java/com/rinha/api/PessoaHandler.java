715e37f8af56e793c5133364c699cc2aae896a9c
eliasjuk <eliasp.juk@hotmail.com>
2024-10-22 21:17:39 -0300

    definir rotas

---
+++ api/src/main/java/com/rinha/api/PessoaHandler.java
@@ -0,0 +1,50 @@
+package com.rinha.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rinha.models.Pessoa;
+import com.sun.net.httpserver.HttpExchange;
+
+public class PessoaHandler extends RequestHandler {
+	private final List<Pessoa> pessoas = new ArrayList<>();
+	private int nextId = 1;
+
+	@Override
+	protected String handleRequest(HttpExchange exchange) throws IOException {
+		switch (exchange.getRequestMethod()) {
+			case "POST":
+				return createPessoa(exchange);
+			case "GET":
+				return getPessoas(exchange);
+			default:
+				return "Método não suportado";
+		}
+	}
+
+	private String createPessoa(HttpExchange exchange) throws IOException {
+		InputStream inputStream = exchange.getRequestBody();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String nome = reader.readLine();
+		Pessoa novaPessoa = new Pessoa(nextId++, nome);
+		pessoas.add(novaPessoa);
+		return "Pessoa criada: ID=" + novaPessoa.getId() + ", Nome=" + novaPessoa.getNome();
+	}
+
+	private String getPessoas(HttpExchange exchange) {
+		StringBuilder response = new StringBuilder("Pessoas cadastradas:\n");
+		for (Pessoa p : pessoas) {
+			response.append("ID: ").append(p.getId()).append(", Nome: ").append(p.getNome()).append("\n");
+		}
+		return response.toString();
+	}
+
+	@Override
+	protected int getResponseCode() {
+		return 200;
+	}
+}
